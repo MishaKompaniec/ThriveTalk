59dcde752ba498d8a78d410f49f96496e5121e4c
Misha Kompaniec <mikokompaniec@gmail.com>
2022-07-26 13:55:00 +0300

    smooth scroll

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let intro = document.querySelector(".intro");
 let header = document.querySelector(".header");
 let cross = document.querySelector(".modalWindow__cross");
 let lining = document.querySelector(".lining");
-let link = document.querySelector(".testing__link");
 let alertWindow = document.querySelector(".alertWindow");
 let alertButton = document.querySelector(".alertWindow__button");
 
@@ -32,10 +31,39 @@ alertButton.addEventListener("click", function () {
 	lining.classList.toggle("active");
 })
 
-link.addEventListener("click", function (e) {
+let linkContact = document.querySelector(".testing__link");
+let linkAbout = document.querySelector("#about");
+let linkServices = document.querySelector("#services");
+let linkBlog = document.querySelector("#blog");
+
+linkContact.addEventListener("click", function (e) {
 	e.preventDefault();
 	window.scrollTo({
 		top: document.querySelector('.footer__info').offsetTop,
+		behavior: "smooth"
+	});
+})
+
+linkAbout.addEventListener("click", function (e) {
+	e.preventDefault();
+	window.scrollTo({
+		top: document.querySelector('.aboutUs').offsetTop,
+		behavior: "smooth"
+	});
+})
+
+linkServices.addEventListener("click", function (e) {
+	e.preventDefault();
+	window.scrollTo({
+		top: document.querySelector('.categories').offsetTop,
+		behavior: "smooth"
+	});
+})
+
+linkBlog.addEventListener("click", function (e) {
+	e.preventDefault();
+	window.scrollTo({
+		top: document.querySelector('.statistics').offsetTop,
 		behavior: "smooth"
 	});
 })
